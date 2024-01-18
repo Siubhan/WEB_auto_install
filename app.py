7fb440ddc701084fc7702dad4a4f273b135182d6
Аден <29686180+Siubhan@users.noreply.github.com>
2024-01-19 02:00:31 +1000

    lost key

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, session
 # создание экземпляра объекта приложения
 app = Flask(__name__)
 # установим секретный ключ для подписи.
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # здесь необходимо указать все контроллеры страниц
 # закомментировать еще не реализованные
 import controllers.client_index
